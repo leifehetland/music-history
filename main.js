fd272f9e79b863b86101c3980b082a95ae85d6b1
Leif E. Hetland <lehetland1@gmail.com>
2015-08-12 20:53:50 -0500

    added jquery and json to music history

--- main.js
+++
@@ -1,9 +0,0 @@
-
-
-var songs = [];
-
-songs[songs.length] = "Idioteque - by Radiohead on the album Kid A";
-songs[songs.length] = "Ramble On - by Led Zeppelin on the album II";
-songs[songs.length] = "Njósnavélin - by Sigur Ros on the album ()";
-songs[songs.length] = "Dancing Mad - by Nobuo Uematsu on the album Final Fantasy VI Original Sound Version";
-songs[songs.length] = "The Girl Who Fell from the Sky - by Joe Hisaishi on the album Laputa: Castle in the Sky";
--- music.js
+++ music.js
@@ -1,22 +1,27 @@
-var songs = [];
-
-var songList = document.getElementById("music_project");
-
-songs[songs.length] = "Legs > by Z*Z Top on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-songs.unshift("Idioteque > by Radiohead on the album Kid A");
-songs.push("Ramble On > by Led Zeppelin on the album II");
+$(document).ready(function() {
+	
+	
+		
+	
 
 
-for (var i = 0; i < songs.length; i++) {
-	var song = songs[i];
-	song = song.replace(/[&\/\\#,+(!)$~%.'":&*@?<{}]/g, '');
-	song = song.replace(">", "-");
-	songList.innerHTML += song + "<br>";
+	$.ajax({
+		url:"./moresongs.json"
+	}).done(function(songList){
+		console.log(songList.songs);
 
-}
+	for (var i = 0; i < songList.songs.length; i++) {
+		var currentSong = songList.songs[i];
+		$("#listOfSongs").append("<h1>" + currentSong.title + "</h1>");
+		$("#listOfSongs").append("<div>Performed by " + currentSong.artist +"</div>");
+		$("#listOfSongs").append("<div>On the album " + currentSong.album + "</div>");
+		
+	};
 
+	
+
+	
+
+});
+
+});
