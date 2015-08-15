fcd9bad371462da20faf3873d902e6b671c24393
Leif E. Hetland <lehetland1@gmail.com>
2015-08-15 12:56:17 -0500

    changes to js

--- music.js
+++ music.js
@@ -1,27 +1,28 @@
-$(document).ready(function() {
-	
-	
-		
-	
-
-
-	$.ajax({
-		url:"./moresongs.json"
-	}).done(function(songList){
-		console.log(songList.songs);
-
+  $(document).ready(function() {
+	function songReturn(songList){
 	for (var i = 0; i < songList.songs.length; i++) {
 		var currentSong = songList.songs[i];
-		$("#listOfSongs").append("<h1>" + currentSong.title + "</h1>");
-		$("#listOfSongs").append("<div>Performed by " + currentSong.artist +"</div>");
-		$("#listOfSongs").append("<div>On the album " + currentSong.album + "</div>");
-		
-	};
-
+		var showSongs = "<li>";
+		showSongs += "<h2>" + currentSong.title + "</h2>";
+		showSongs += "<div>Performed by " + currentSong.artist +"</div>";
+		showSongs += "<div>On the album " + currentSong.album + "</div>";
+		showSongs += "<button id='delete'>Remove Song</button>";
+		showSongs += "</l1>";
+		$("#listOfSongs").append(showSongs);	
+	}
+}
 	
+		$.ajax({
+		url:"./songs.json"
+	}).done(songReturn);
 
-	
+	$(document).on("click", "#delete", function() {
+		$(this).parent().hide();
+  });
 
-});
-
+	 $("#addSongs").click(function(){
+        $.ajax({
+            url:"./moresongs.json"
+        }).done(songReturn);
+    });
 });
